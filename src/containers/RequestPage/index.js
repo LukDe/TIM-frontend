600ecf3cbbfc03824a44bb23d555ef9addf58120
Max Franke <m.franke@campus.tu-berlin.de>
2016-06-28 20:54:59 +0200

    RequestPage fixed

--- src/containers/RequestPage/index.js
+++ src/containers/RequestPage/index.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import R from 'ramda'
 
+import SelectStreetModal from '../../components/SelectStreetModal'
 import * as AC from '../../actions/request'
 import { requestValidation } from './validation'
 import Api from '../App/api'
@@ -14,10 +15,18 @@ class RequestPage extends Component {
       username: this.props.userData,
       goodName: '',
       misc: '',
+      active: true,
       quantity: '-1',
       range: '',
       postalCode: '',
       priority: '',
+      address: {
+        name: '',
+        coords: {
+          latitude: '',
+          longitude: ''
+        }
+      },
       labelText: "Wie viel Liter Wasser brauchen Sie?",
       placeholderText: "Liter",
       miscHidden: true
@@ -33,6 +42,25 @@ class RequestPage extends Component {
       }
       return false
     })
+  }
+
+  handleChange (prop) {
+    return (event) => {
+      this.setState({ [prop]: event.target.value }) // eslint-disable-line
+    }
+  }
+
+  handleAddress (address) {
+    this.setState({ // eslint-disable-line
+      address: {
+        name: address.formatted_address,
+        coords: {
+          latitude: address.geometry.location.lat,
+          longitude: address.geometry.location.lng
+        }
+      }
+    })
+    $('#select-street-modal').modal('hide')
   }
 
   handleGps (selection) {
@@ -62,12 +90,6 @@ class RequestPage extends Component {
           }
           break
       }
-    }
-  }
-
-  handleChange (prop) {
-    return (event) => {
-      this.setState({ [prop]: event.target.value }) // eslint-disable-line
     }
   }
 
@@ -102,109 +124,117 @@ class RequestPage extends Component {
   }
   render () {
     return (
-      <form id="offer-form" className="ui form">
-        <div className="fields">
-          <label>Was brauchen Sie?</label>
-          <div className="field">
-            <div className="ui radio checkbox">
-              <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("water")}
-                     type="radio" name="goodName" value="water"/>
-              <label><img src={require('../../img/water.svg')} className="image" /></label>
+      <div>
+        <SelectStreetModal onClick={this.handleAddress.bind(this)} />
+        <form id="offer-form" className="ui form">
+          <div className="fields">
+            <label>Was brauchen Sie?</label>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("water")}
+                       type="radio" name="goodName" value="water"/>
+                <label><img src={require('../../img/water.svg')} className="image" /></label>
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("food")}
+                       type="radio" name="goodName" value="food"/>
+                <label><img src={require('../../img/food.svg')} className="image" /></label>
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("woundcare")}
+                       type="radio" name="goodName" value="woundcare"/>
+                <label><img src={require('../../img/woundcare.svg')} className="image" /></label>
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("clothes")}
+                       type="radio" name="goodName" value="clothes"/>
+                <label><img src={require('../../img/clothes.svg')} className="image" /></label>
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("accomodation")}
+                       type="radio" name="goodName" value="accomodation"/>
+                <label><img src={require('../../img/accomodation.svg')} className="image" /></label>
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input onChange={this.handleChange('goodName')} onClick ={this.labelUpdater("other")}
+                       type="radio" name="goodName" value="other"/>
+                <label><img src={require('../../img/other.svg')} className="image" /></label>
+              </div>
             </div>
           </div>
-          <div className="field">
-            <div className="ui radio checkbox">
-              <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("food")}
-                     type="radio" name="goodName" value="food"/>
-              <label><img src={require('../../img/food.svg')} className="image" /></label>
-            </div>
-          </div>
-          <div className="field">
-            <div className="ui radio checkbox">
-              <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("woundcare")}
-                     type="radio" name="goodName" value="woundcare"/>
-              <label><img src={require('../../img/woundcare.svg')} className="image" /></label>
-            </div>
-          </div>
-          <div className="field">
-            <div className="ui radio checkbox">
-              <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("clothes")}
-                     type="radio" name="goodName" value="clothes"/>
-              <label><img src={require('../../img/clothes.svg')} className="image" /></label>
-            </div>
-          </div>
-          <div className="field">
-            <div className="ui radio checkbox">
-              <input onChange={this.handleChange('goodName')} onClick={this.labelUpdater("accomodation")}
-                     type="radio" name="goodName" value="accomodation"/>
-              <label><img src={require('../../img/accomodation.svg')} className="image" /></label>
-            </div>
-          </div>
-          <div className="field">
-            <div className="ui radio checkbox">
-              <input onChange={this.handleChange('goodName')} onClick ={this.labelUpdater("other")}
-                     type="radio" name="goodName" value="other"/>
-              <label><img src={require('../../img/other.svg')} className="image" /></label>
-            </div>
-          </div>
-        </div>
 
-        {this.state.miscHidden
-           ? ''
-           : (<div className="field hidden">
-               <label>Was?</label>
-               <input onChange={this.handleChange('misc')} id = "misc"
-                 type="text" name="misc" placeholder= "Was brauchen sie?"/>
+          {this.state.miscHidden
+             ? ''
+             : (<div className="field hidden">
+                 <label>Was?</label>
+                 <input onChange={this.handleChange('misc')} id = "misc"
+                   type="text" name="misc" placeholder= "Was brauchen sie?"/>
+                </div>)
+          }
+          {this.state.miscHidden
+            ? (<div className="field">
+                <label>{this.state.labelText}</label>
+                <input onChange={this.handleChange('quantity')}
+                       type="text" name="quantity" placeholder={this.state.placeholderText}/>
               </div>)
-        }
-        {this.state.miscHidden
-          ? (<div className="field">
-              <label>{this.state.labelText}</label>
-              <input onChange={this.handleChange('quantity')}
-                     type="text" name="quantity" placeholder={this.state.placeholderText}/>
-            </div>)
-          : ''
-        }
+            : ''
+          }
 
-        <div className="field">
-          <label>Wo?</label>
-          <input onChange={this.handleChange('postalCode')}
-                 type="text" name="postalCode" placeholder="Postleitzahl"/>
-        </div>
-
-        <div className="field">
-          <label>In welchem Umkreis?</label>
-          <input onChange={this.handleChange('range')}
-                 type="text" name="range" placeholder="Umkreis"/>
-        </div>
-
-        <div className="fields">
-          <label>Dringlichkeit</label>
-            <div className="field">
-              <div className="ui radio checkbox">
-                <input onChange={this.handleChange('priority')}
-                       type="radio" name="priority" value="1"/>
-                <label>1 Tag</label>
-              </div>
-            </div>
-            <div className="field">
-              <div className="ui radio checkbox">
-                <input onChange={this.handleChange('priority')}
-                       type="radio" name="priority" value="2"/>
-                <label>2 Tage</label>
-              </div>
-            </div>
-            <div className="field">
-              <div className="ui radio checkbox">
-                <input onChange={this.handleChange('priority')}
-                       type="radio" name="priority" value="3"/>
-                <label>3 Tage</label>
-              </div>
+          <div className="ui action field">
+            <label>Wo?</label>
+            <input type="text" name="location" value={this.state.address.name}/>
+            <div className="ui buttons">
+              <button onClick={this.handleGps('MAP')} className="ui button">Straße</button>
+              <div className="or" data-text="or"></div>
+              <button onClick={this.handleGps('AUTOMATIC')} className="ui positive button">Automatisch</button>
             </div>
           </div>
-        <button className="ui button" type="submit">Abschicken</button>
-        <div className="ui error message"></div>
-      </form>)
+
+          <div className="field">
+            <label>In welchem Umkreis?</label>
+            <input onChange={this.handleChange('range')}
+                   type="text" name="range" placeholder="Umkreis"/>
+          </div>
+
+          <div className="fields">
+            <label>Dringlichkeit</label>
+              <div className="field">
+                <div className="ui radio checkbox">
+                  <input onChange={this.handleChange('priority')}
+                         type="radio" name="priority" value="1"/>
+                  <label>1 Tag</label>
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui radio checkbox">
+                  <input onChange={this.handleChange('priority')}
+                         type="radio" name="priority" value="2"/>
+                  <label>2 Tage</label>
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui radio checkbox">
+                  <input onChange={this.handleChange('priority')}
+                         type="radio" name="priority" value="3"/>
+                  <label>3 Tage</label>
+                </div>
+              </div>
+            </div>
+          <button className="ui button" type="submit">Abschicken</button>
+          <div className="ui error message"></div>
+        </form>
+      </div>
+    )
   }
 }
 
@@ -222,7 +252,7 @@ const mapDispatchToProps = (dispatch) => ({
       misc: state.misc,
       quantity: state.quantity,
       range: state.range,
-      postalCode: state.postalCode,
+      location: `${state.address.coords.latitude},${state.address.coords.longitude}`,
       priority: state.priority
     }
     dispatch(AC.requestNew(payload))
